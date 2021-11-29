df97ab6b6702e24a7e133ef1ea37cee6912e28f5
SafaaQutub18 <51971679+SafaaQutub@users.noreply.github.com>
2021-11-29 10:10:26 +0300

    update the names of app

--- GuessPhraseApp/app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
+++ GuessPhraseApp/app/src/main/java/com/example/recyclerviewapp/MainActivity.kt
@@ -1,102 +1,179 @@
 package com.example.recyclerviewapp
 
+import android.content.Context
 import android.content.DialogInterface
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AlertDialog
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+
+    lateinit var scoreText: TextView
+    lateinit var phraseText: TextView
     lateinit var recyclerV: RecyclerView
     lateinit var newGusseNumET: EditText
     lateinit var sendBt: Button
 
-    var gameTextList: ArrayList<GameText> = ArrayList()
-    val gameAdapterRV: GameAdapter by lazy { GameAdapter() }
+    var guessesTextList: ArrayList<GuessesText> = ArrayList()
+    val phraseAdapterRV: PhraseAdapter by lazy { PhraseAdapter() }
 
-    //game var:
-    var chancesNum = 3
-    var random = kotlin.random.Random.nextInt(0, 11)
+    //Phrase var:
+    var myPhrase = "this is the secret phrase"
+    //var starsString = "**** ** *** ****** ******"
+    var starsString = ""
+    var unguessedletter = 9
+    var lossCount = 0
+    var guessedLetter = ""
+    var resultText = ""
+    var currentScore = 0
+    var highest_Score = 0
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        for(i in 0 until myPhrase.length){
+            if(myPhrase[i] != ' ')
+                starsString += "*"
+            else
+                starsString += " "
+        }
+
         recyclerV = findViewById(R.id.rvGameText)
         newGusseNumET = findViewById(R.id.editTextNewNumber)
         sendBt = findViewById(R.id.sendBt)
+        phraseText = findViewById(R.id.phraseTV)
+        scoreText = findViewById(R.id.scoreTV)
+        recyclerV.adapter = phraseAdapterRV
 
-        recyclerV.adapter = gameAdapterRV
+        // git the highest Score from Shared Preferences
+        highest_Score =  getHighestScore()
+        scoreText.text = "Highest Score: $highest_Score"
 
+        phraseText.text = "Phrase: $starsString\nGuessed letters:"
 
 
         sendBt.setOnClickListener {
-            val userNum = Integer.parseInt(newGusseNumET.text.toString())
-            checkNumber(userNum)
-            gameAdapterRV.setGameList(gameTextList)
+            val userGuess = newGusseNumET.text.toString()
+            if(lossCount < 10)
+                checkGuess(userGuess)
+            else{
+                resultText = "You loss T.T"
+                gameOverAlert()
+            }
+            //save score
+
+            saveScore(currentScore)
+            scoreText.text = "Highest Score: $highest_Score"
+
+            phraseText.text = "Phrase: $starsString\nGuessed letters: $guessedLetter "
+            phraseAdapterRV.setPhraseList(guessesTextList)
             newGusseNumET.setText("") // نفضي الedit text
 
-            //gameTextList.add(GameText())
 
         }
     }
 
-    fun checkNumber(userGusse: Int) {
-        var finalText: String
-        if (chancesNum > 0) {
-            gameTextList.add(GameText("you guessed $userGusse"))
+    fun checkGuess(userGusse: String) {
+        if (userGusse.length > 1) {
             when (userGusse) {
-                random -> {
-                    println("you got it *_*")
-                    gameTextList.add(GameText("you got it *_* , correct answer"))
-                    chancesNum = 0
+                myPhrase -> {
+                    currentScore += 10
+                    resultText = "You win!"
+                    gameOverAlert()
                 }
                 else -> {
-                    println("wrong guess")
-                    chancesNum--
-                    gameTextList.add(GameText("wrong guess, you have $chancesNum guesses left"))
+                    if(currentScore>0) currentScore--
+                    lossCount++
+                    guessesTextList.add(GuessesText("wrong guess: $userGusse ","#FA2500"))
                 }
             }
         } else {
-            //gameTextList.add(GameText("Game over"))
-            gameOverAlert()
+            if(myPhrase.contains(userGusse))
+                checkLetter(userGusse)
+            else {
+                if(currentScore>0) currentScore--
+                lossCount++
+                guessesTextList.add(GuessesText("wrong guess: $userGusse","#FA2500"))
+            }
         }
 
-    }
 
+    }
+    fun checkLetter(letter: String) {
+        var foundCount = 0
+        // check if the guess completed
+        if (starsString.contains('*')) {
+            //check if the letter not entered previously
+            if(!guessedLetter.contains(letter)) {
+                guessedLetter += (letter + ",")
+                unguessedletter--
+                currentScore++
+            }
+            var index = myPhrase.indexOf(letter)
+            while (index >= 0) {
+                foundCount++
+                starsString[index] = letter
+                val chars = starsString.toCharArray()
+                chars[index] = letter.single()
+                starsString = String(chars)
+                index = myPhrase.indexOf(letter, index + 1);
+            }
+            guessesTextList.add(GuessesText("found $foundCount $letter(s)","#33D402"))
+            guessesTextList.add(GuessesText("$unguessedletter guess remaining","#010300"))
+        } else {
+            resultText = "You win!"
+            gameOverAlert()
+        }
+        if(unguessedletter == 0){
+            resultText = "You win!"
+            gameOverAlert()
+        }
+    }
+    fun getHighestScore(): Int{
+        val highestScore : Int
+        sharedPreferences = this.getSharedPreferences(
+            getString(R.string.preference_file_key), Context.MODE_PRIVATE)
+        highestScore = Integer.parseInt(sharedPreferences.getString("score", "0").toString())  // --> retrieves data from Shared Preferences
+        return highestScore
+    }
+    fun saveScore(score: Int){
+        if(score > getHighestScore()){
+        // We can save data with the following code
+        with(sharedPreferences.edit()) {
+            putString("score", score.toString())
+            apply()
+        }
+            this.highest_Score = score
+    }
+    }
     fun gameOverAlert() {
         // first we create a variable to hold an AlertDialog builder
         val dialogBuilder = AlertDialog.Builder(this)
-        // then we set up the input
-        // val input = EditText(this)
-        // here we set the message of our alert dialog
-        dialogBuilder.setMessage("Game over")
+        dialogBuilder.setMessage("$resultText\nYour scores:$currentScore")
             // positive button text and action
-            .setPositiveButton("play again", DialogInterface.OnClickListener { dialog, id ->
-
-                this.gameTextList.clear()
-
-                //game var:
-                this.chancesNum = 3
-                this.random = kotlin.random.Random.nextInt(0, 11)
-                gameAdapterRV.setGameList(gameTextList)
-                newGusseNumET.setText("") // نفضي الedit text
-
-            })
+            .setPositiveButton("Play again", DialogInterface.OnClickListener { dialog, id -> this.recreate() })
             // negative button text and action
-            .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id ->
-                dialog.cancel()
-            })
+            .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id -> dialog.cancel()})
         // create dialog box
         val alert = dialogBuilder.create()
         // set title for alert dialog box
-        alert.setTitle("Game overc")
+        alert.setTitle("Game over")
         // add the Edit Text
         //alert.setView(input)
         // show alert dialog
         alert.show()
     }
+}
+
+private operator fun String.set(i: Int, value: String) {
+
 }
